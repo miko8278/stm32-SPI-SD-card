aae06b983e4749b0c71114cbbea9c999cfab6f4b
miko8278 <154246065+miko8278@users.noreply.github.com>
2026-07-18 19:59:09 +0200

    generalising for SPIx, using templates and more cpp

--- FatFS/spi-sdcardwrite/main.cpp
+++ FatFS/spi-sdcardwrite/main.cpp
@@ -1,16 +1,95 @@
 #include "stm32g431xx.h"
 
-
+//old def
 #define SD_CS_SELECT()     (GPIOA->BSRR = (1U << (9 + 16))) // PA9 LOW
 #define SD_CS_DESELECT()   (GPIOA->BSRR = (1U << 9))        // PA9 HIGH
 
 
-static void GPIO_Init(void)
+template<uintptr_t PortBase, uint32_t Pin>
+struct ChipSelect {
+private:
+    static inline GPIO_TypeDef* const port = reinterpret_cast<GPIO_TypeDef*>(PortBase);
+public:
+    ChipSelect() {
+        port->BSRR = (1U << (Pin + 16));
+    }
+
+    ~ChipSelect() {
+        port->BSRR = (1U << Pin);
+    }
+};
+
+
+//static class
+template<uintptr_t Base>
+struct SpiDriver {
+
+    SpiDriver() = delete;
+    private:
+    static inline SPI_TypeDef* const SPIx = reinterpret_cast<SPI_TypeDef*>(Base);
+
+    //Each SPI-Device needs clock enabled seperately
+    static void EnableClock() {
+        if constexpr (Base == SPI1_BASE) {
+            RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+        }
+        else if constexpr (Base == SPI2_BASE) {
+            RCC->APB1ENR1 |= RCC_APB1ENR1_SPI2EN;
+        }
+        else if constexpr (Base == SPI3_BASE) {
+            RCC->APB1ENR1 |= RCC_APB1ENR1_SPI3EN;
+        }
+    }
+
+    public:
+    static void Init() {
+        EnableClock();
+        SPIx->CR1 = 0;
+        SPIx->CR2 = 0;
+
+        // Master, Software NSS, Baud = APBCLK / 128
+        SPIx->CR1 = SPI_CR1_MSTR | SPI_CR1_SSM | SPI_CR1_SSI | (6U << SPI_CR1_BR_Pos);
+        
+        // 8-Bit
+        SPIx->CR2 = (7U << SPI_CR2_DS_Pos) | SPI_CR2_FRXTH;
+        
+        SPIx->CR1 |= SPI_CR1_SPE;
+    }
+
+    static void SetHighSpeed() {
+        SPIx->CR1 &= ~SPI_CR1_SPE;
+        SPIx->CR1 &= ~SPI_CR1_BR_Msk;
+        SPIx->CR1 |= (3U << SPI_CR1_BR_Pos); // z.B. /16
+        SPIx->CR1 |= SPI_CR1_SPE;
+    }
+
+    static uint8_t Transfer(uint8_t data) {
+        // wait till tx empty
+        while (!(SPIx->SR & SPI_SR_TXE));
+
+        //strange 8-Bit cast
+        *(__IO uint8_t *)&SPIx->DR = data;
+
+        // wait will rx not empty
+        while (!(SPIx->SR & SPI_SR_RXNE));
+
+        return *(__IO uint8_t *)&SPIx->DR;
+    }
+};
+
+/**
+ * @brief Initialises the needed GPIOs. It's here 
+ * where the SPI Pins are being chosen.
+ */
+static void GPIO_Init()
 {
     // Enable clocks
     RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
-    RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOBEN;
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;
+    //RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
 
+    //SPI1 chosen PINs
     // PA5 = SCK, PA6 = MISO ,PA7 = MOSI (Alternate Function)
     GPIOA->MODER &= ~(GPIO_MODER_MODE5_Msk | GPIO_MODER_MODE6_Msk | GPIO_MODER_MODE7_Msk);
     GPIOA->MODER |=
@@ -24,6 +103,25 @@ static void GPIO_Init(void)
         (5U << GPIO_AFRL_AFSEL5_Pos) |
         (5U << GPIO_AFRL_AFSEL6_Pos) |
         (5U << GPIO_AFRL_AFSEL7_Pos);
+
+    //SPI2 chosen PINs
+    // PB3 = SCK PB4 = MISO PB5 = MOSI -> Alternate Function mode
+    GPIOB->MODER &= ~(GPIO_MODER_MODE3_Msk | GPIO_MODER_MODE4_Msk | GPIO_MODER_MODE5_Msk);
+    GPIOB->MODER |=
+        (2U << GPIO_MODER_MODE3_Pos) |
+        (2U << GPIO_MODER_MODE4_Pos) |
+        (2U << GPIO_MODER_MODE5_Pos);
+
+    // PB3/PB4/PB5 -> Alternate Function mode
+    GPIOB->MODER &= ~(GPIO_MODER_MODE3_Msk |
+                    GPIO_MODER_MODE4_Msk |
+                    GPIO_MODER_MODE5_Msk);
+
+    GPIOB->MODER |=
+        (2U << GPIO_MODER_MODE3_Pos) |
+        (2U << GPIO_MODER_MODE4_Pos) |
+        (2U << GPIO_MODER_MODE5_Pos);
+
 
     // Push-pull, this is standard actually, but somewhere I have to use open-drain I think
     GPIOA->OTYPER &= ~((1U << 4) | (1U << 5) | (1U << 7) | (1U << 9));
@@ -53,92 +151,35 @@ static void GPIO_Init(void)
     GPIOA->OSPEEDR |= (2U << GPIO_OSPEEDR_OSPEED4_Pos);
 }
 
-static void SPI1_Init(void)
-{
-    // Disable SPI while configuring
-    SPI1->CR1 = 0;
-    SPI1->CR2 = 0;
-
-    // Master mode
-    // Software NSS
-    // Baud = APB2CLK / 128 => 125KHz
-    //We need to send at least 74 cycles of slow data
-    //for the sd-card init => 10 8bit messages
-    SPI1->CR1 =
-          SPI_CR1_MSTR
-        | SPI_CR1_SSM
-        | SPI_CR1_SSI
-        | (6U << SPI_CR1_BR_Pos);
-
-    // 8-bit transfers, mystical FRXTH
-    SPI1->CR2 =
-          (7U << SPI_CR2_DS_Pos)
-        | SPI_CR2_FRXTH;
-
-    // Enable SPI
-    SPI1->CR1 |= SPI_CR1_SPE;
-}
-
-// static void SPI1_SendByte(uint8_t data)
-// {
-//     while (!(SPI1->SR & SPI_SR_TXE));
-
-
-//     //DR is a 16 bit register, to send 8bit one possible
-//     //way is to use this super weird cast
-//     *(__IO uint8_t *)&SPI1->DR = data;
-
-//     while (SPI1->SR & SPI_SR_BSY);
-// }
-
-static void SPI1_SetHighSpeed()
-{
-    SPI1->CR1 &= ~SPI_CR1_SPE; // SPI off
-    SPI1->CR1 &= ~SPI_CR1_BR_Msk;
-    SPI1->CR1 |= (3U << SPI_CR1_BR_Pos); // /16
-    SPI1->CR1 |= SPI_CR1_SPE;  // SPI on
-}
-
-static uint8_t SPI1_Transfer(uint8_t data)
-{
-    // w8 till txe empty
-    while (!(SPI1->SR & SPI_SR_TXE));
-
-    // write data
-    *(__IO uint8_t *)&SPI1->DR = data;
-
-    // wait till rxe empty
-    while (!(SPI1->SR & SPI_SR_RXNE));
-
-    // return the received byte
-    return *(__IO uint8_t *)&SPI1->DR;
-}
-
-
-
+template<uintptr_t Base, uintptr_t PortBase, uint32_t Pin>
 uint8_t SD_SendCmd(uint8_t cmd, uint32_t arg, uint8_t crc, uint8_t *response_buf, uint8_t extra_bytes)
 {
+
+    //This gets automatically destructed when leaving the function at any point
+    ChipSelect<PortBase,Pin> chipselect_tmp;
+
+
     uint8_t r1;
     uint16_t timeout = 0;
 
     //chip select
-    SD_CS_SELECT();
+    //SD_CS_SELECT();
 
     // send cmd
-    SPI1_Transfer(cmd | 0x40);
+    SpiDriver<Base>::Transfer(cmd | 0x40);
     
     // 4 byte argument
-    SPI1_Transfer((uint8_t)(arg >> 24));
-    SPI1_Transfer((uint8_t)(arg >> 16));
-    SPI1_Transfer((uint8_t)(arg >> 8));
-    SPI1_Transfer((uint8_t)arg);
+    SpiDriver<Base>::Transfer((uint8_t)(arg >> 24));
+    SpiDriver<Base>::Transfer((uint8_t)(arg >> 16));
+    SpiDriver<Base>::Transfer((uint8_t)(arg >> 8));
+    SpiDriver<Base>::Transfer((uint8_t)arg);
     
     // 1 byte CRC (SPI-Mode ignores it, but still needed even then)
-    SPI1_Transfer(crc);
+    SpiDriver<Base>::Transfer(crc);
 
     // wait for first answer byte
     do {
-        r1 = SPI1_Transfer(0xFF);
+        r1 = SpiDriver<Base>::Transfer(0xFF);
         timeout++;
     } while ((r1 == 0xFF) && (timeout < 255));
 
@@ -149,136 +190,143 @@ uint8_t SD_SendCmd(uint8_t cmd, uint32_t arg, uint8_t crc, uint8_t *response_buf
         // when r1 is valid, we put the rest inside the buffer
         if (r1 != 0xFF && extra_bytes > 0) {
             for (uint8_t i = 1; i <= extra_bytes; i++) {
-                response_buf[i] = SPI1_Transfer(0xFF);
+                response_buf[i] = SpiDriver<Base>::Transfer(0xFF);
             }
         }
     }
 
-    SD_CS_DESELECT();
+    //SD_CS_DESELECT();
 
     //Maybe a filler Byte is needed later
-    //SPI1_Transfer(0xFF);
+    //SpiDriver<Base>::Transfer(0xFF);
 
     return r1; //always return r1, even when there is more
 }
 
 
+template<uintptr_t Base, uintptr_t PortBase, uint32_t Pin>
 uint8_t SD_ReadBlock(uint32_t block_addr, uint8_t *buffer)
 {
+    //This gets automatically destructed when leaving the function at any point
+    ChipSelect<PortBase,Pin> chipselect_tmp;
+
     uint8_t r1;
     uint16_t timeout = 0;
 
-    SD_CS_SELECT();
+    //SD_CS_SELECT();
 
     // send CMD17
     // CRC not important anymore => 0xFF
-    SPI1_Transfer(17 | 0x40);
-    SPI1_Transfer((uint8_t)(block_addr >> 24));
-    SPI1_Transfer((uint8_t)(block_addr >> 16));
-    SPI1_Transfer((uint8_t)(block_addr >> 8));
-    SPI1_Transfer((uint8_t)block_addr);
-    SPI1_Transfer(0xFF); 
+    SpiDriver<Base>::Transfer(17 | 0x40);
+    SpiDriver<Base>::Transfer((uint8_t)(block_addr >> 24));
+    SpiDriver<Base>::Transfer((uint8_t)(block_addr >> 16));
+    SpiDriver<Base>::Transfer((uint8_t)(block_addr >> 8));
+    SpiDriver<Base>::Transfer((uint8_t)block_addr);
+    SpiDriver<Base>::Transfer(0xFF); 
 
     // waiting for answer... it has to be 0x00
     timeout = 0;
     do {
-        r1 = SPI1_Transfer(0xFF);
+        r1 = SpiDriver<Base>::Transfer(0xFF);
         timeout++;
     } while ((r1 == 0xFF) && (timeout < 1000));
 
     if (r1 != 0x00) {
-        SD_CS_DESELECT();
+        //SD_CS_DESELECT();
         return 0x01; // did not accept command
     }
 
     // wait for start 0xFE
     timeout = 0;
     do {
-        r1 = SPI1_Transfer(0xFF);
+        r1 = SpiDriver<Base>::Transfer(0xFF);
         timeout++;
     } while ((r1 != 0xFE) && (timeout < 0xFFFF));
 
     if (r1 != 0xFE) {
-        SD_CS_DESELECT();
+        //SD_CS_DESELECT();
         return 0x02; // timeout-error waiting for 0xFE
     }
 
     // read 512 byte blockdata
     for (uint16_t i = 0; i < 512; i++) {
-        buffer[i] = SPI1_Transfer(0xFF);
+        buffer[i] = SpiDriver<Base>::Transfer(0xFF);
     }
 
     // read 2 byte CRC, do nothing with it
-    SPI1_Transfer(0xFF);
-    SPI1_Transfer(0xFF);
+    SpiDriver<Base>::Transfer(0xFF);
+    SpiDriver<Base>::Transfer(0xFF);
 
 
-    SD_CS_DESELECT();
-    SPI1_Transfer(0xFF); // extra cycle
+    //SD_CS_DESELECT();
+    SpiDriver<Base>::Transfer(0xFF); // extra cycle
 
     return 0x00; // success
 }
 
+template<uintptr_t Base, uintptr_t PortBase, uint32_t Pin>
 uint8_t SD_WriteBlock(uint32_t block_addr, const uint8_t *buffer)
 {
+    //This gets automatically destructed when leaving the function at any point
+    ChipSelect<PortBase,Pin> chipselect_tmp;
     uint8_t r1;
     uint16_t timeout = 0;
 
-    SD_CS_SELECT();
+    //SD_CS_SELECT();
 
     // send CMD24
-    SPI1_Transfer(24 | 0x40);
-    SPI1_Transfer((uint8_t)(block_addr >> 24));
-    SPI1_Transfer((uint8_t)(block_addr >> 16));
-    SPI1_Transfer((uint8_t)(block_addr >> 8));
-    SPI1_Transfer((uint8_t)block_addr);
-    SPI1_Transfer(0xFF); // CRC doesn't matter
+    SpiDriver<Base>::Transfer(24 | 0x40);
+    SpiDriver<Base>::Transfer((uint8_t)(block_addr >> 24));
+    SpiDriver<Base>::Transfer((uint8_t)(block_addr >> 16));
+    SpiDriver<Base>::Transfer((uint8_t)(block_addr >> 8));
+    SpiDriver<Base>::Transfer((uint8_t)block_addr);
+    SpiDriver<Base>::Transfer(0xFF); // CRC doesn't matter
 
 
     // wait for answer 0x00
     timeout = 0;
     do {
-        r1 = SPI1_Transfer(0xFF);
+        r1 = SpiDriver<Base>::Transfer(0xFF);
         timeout++;
     } while ((r1 == 0xFF) && (timeout < 1000));
 
     if (r1 != 0x00) {
-        SD_CS_DESELECT();
+        //SD_CS_DESELECT();
         return 0x01; // Error: cmd not accepted
     }
 
     // one cycle for sync
-    SPI1_Transfer(0xFF);
+    SpiDriver<Base>::Transfer(0xFF);
 
     // send start sign for single-block-write
-    SPI1_Transfer(0xFE);
+    SpiDriver<Base>::Transfer(0xFE);
 
     // send 512 byte blockdata
     for (uint16_t i = 0; i < 512; i++) {
-        SPI1_Transfer(buffer[i]);
+        SpiDriver<Base>::Transfer(buffer[i]);
     }
 
     // 2 byte dummy crc
-    SPI1_Transfer(0xFF);
-    SPI1_Transfer(0xFF);
+    SpiDriver<Base>::Transfer(0xFF);
+    SpiDriver<Base>::Transfer(0xFF);
 
     // data response from card
-    r1 = SPI1_Transfer(0xFF);
+    r1 = SpiDriver<Base>::Transfer(0xFF);
     if ((r1 & 0x1F) != 0x05) {
         // data not accepted
-        SD_CS_DESELECT();
+        //SD_CS_DESELECT();
         return 0x02; 
     }
 
     // wait as long as SD-card is writing
     timeout = 0;
     do {
-        r1 = SPI1_Transfer(0xFF);
+        r1 = SpiDriver<Base>::Transfer(0xFF);
         timeout++;
     } while ((r1 == 0x00) && (timeout < 0xFFFF));
 
-    SD_CS_DESELECT();
-    SPI1_Transfer(0xFF); // // extra cycle
+    //SD_CS_DESELECT();
+    SpiDriver<Base>::Transfer(0xFF); // // extra cycle
 
     if (r1 == 0x00) {
         return 0x03; // timeout while writing
@@ -300,24 +348,24 @@ int main()
     uint16_t timeout = 0;
 
     GPIO_Init();
-    SPI1_Init();
-
+    //SPI1_Init();
+    SpiDriver<SPI1_BASE>::Init();
     SD_CS_DESELECT();
     for (uint8_t i = 0; i < 10; i++){
-        SPI1_Transfer(0xFF);
+        SpiDriver<SPI1_BASE>::Transfer(0xFF);
     }
 
     while (1)
     {
         timeout = 0;
         //CMD0
-        cmd0_resp = SD_SendCmd(0, 0, 0x95, nullptr, 0);
+        cmd0_resp = SD_SendCmd<SPI1_BASE, GPIOA_BASE,9>(0, 0, 0x95, nullptr, 0);
         if(cmd0_resp != 0x01){
             GPIOA->ODR |= (1U << 4); 
             continue;
         }
         //CMD8
-        SD_SendCmd(8, 0x000001AA, 0x87, cmd8_resp, 4);
+        SD_SendCmd<SPI1_BASE, GPIOA_BASE,9>(8, 0x000001AA, 0x87, cmd8_resp, 4);
         if (cmd8_resp[0] != 0x01 && cmd8_resp[4] != 0xAA){
             GPIOA->ODR |= (1U << 4); 
             continue;
@@ -326,12 +374,12 @@ int main()
         //CMD55 + ACMD41
         do {
             // send CMD55, always needed before sending an application command
-            cmd55_resp = SD_SendCmd(55, 0, 0xFF, nullptr, 0);
+            cmd55_resp = SD_SendCmd<SPI1_BASE, GPIOA_BASE,9>(55, 0, 0xFF, nullptr, 0);
 
             if (cmd55_resp <= 0x01) 
             {
                 // argument: 0x40000000 (HCS-Bits set -> support for SDHC/SDXC, Blockadressing instead of Byteadressing)
-                acmd41_resp = SD_SendCmd(41, 0x40000000, 0xFF, nullptr, 0);
+                acmd41_resp = SD_SendCmd<SPI1_BASE, GPIOA_BASE, 9>(41, 0x40000000, 0xFF, nullptr, 0);
             }
 
             timeout++;
@@ -352,31 +400,16 @@ int main()
             sd_block_buffer[i] = (uint8_t)i; 
         }
 
-        uint8_t write_result = SD_WriteBlock(10, sd_block_buffer);
+        uint8_t write_result = SD_WriteBlock<SPI1_BASE, GPIOA_BASE,9>(10, sd_block_buffer);
 
         if (write_result == 0x00) {
             // read block again to verify
-            uint8_t read_back_result = SD_ReadBlock(10, sd_block_buffer2);
+            uint8_t read_back_result = SD_ReadBlock<SPI1_BASE, GPIOA_BASE, 9>(10, sd_block_buffer2);
             
             if (read_back_result == 0x00) {
                 __BKPT(0); // debugger softbreakpoint
             }
         }
-
-
-        // if (read_result == 0x00)
-        // {
-
-        //     if (sd_block_buffer[510] == 0x55 && sd_block_buffer[511] == 0xAA)
-        //     {
-        //         // MBR signature
-        //         GPIOA->ODR ^= (1U << 4); // LED toggle
-        //     }
-        // }
-        // else
-        // {
-        //     GPIOA->ODR |= (1U << 4); 
-        // }
 
 
         for (volatile uint32_t i = 0; i < 10000; i++);
